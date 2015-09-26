2359a35f4dbbcefbcc674346aa9a3d4fd3f4cb8c
Daniel Ram <danieldram@gmail.com>
2015-09-26 06:23:47 -0700

    fix phantomjs error

--- src/inherit-prototypes.js
+++ src/inherit-prototypes.js
@@ -1,13 +1,11 @@
 function inherit (extendthis, fromthese) {
-  console.log(fromthese)
-  console.log(extendthis)
   var keys=[];
   if(extendthis.prototype == undefined){
       var Empty = function(){}
       Empty.prototype = Object.prototype;
       extendthis.prototype = Empty.prototype;
   }
-  fromthese.map((fromthis)=>{
+  fromthese.map(function(fromthis){
     keys = Object.getOwnPropertyNames(fromthis.prototype);
     keys.shift();
     keys.map(function(key){
--- tests/test.js
+++ tests/test.js
@@ -7,14 +7,18 @@ var inherit = require('../src/inherit-prototypes');
 describe('ES6 class extends some class prorotypes', function () {
   it('should extend some ES6 style classes', function(){
     class base extends Array {}
+    class Extras{
+      extra(){return 'hi';}
+    }
 
-    var dependencies = [String, Object];
+    var dependencies = [String, Object, Extras];
     inherit(base, dependencies);
 
     var arr = new base();
     arr.push('lets extend stuff in es6');
     expect(arr[0]).equals('lets extend stuff in es6');
     expect(arr[0].toUpperCase()).equals('LETS EXTEND STUFF IN ES6')
+    expect(arr.extra()).equals('hi');
 
   });
 });
